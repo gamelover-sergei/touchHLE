7bcc62ee889d875ec653d67647b90dd94f2d8ee8
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-08 10:14:36 +0200

    Update i_simulate.rs

--- src/frameworks/uikit/i_simulate.rs
+++ src/frameworks/uikit/i_simulate.rs
@@ -1,10 +1,14 @@
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{msg, objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation iSimulate: UIView
+- (bool)disableTouchesOverlay:(id)defaultName {
+    let val: id = msg![env; this objectForKey:defaultName];
+    msg![env; val boolValue]
+}
 
 @end
 
