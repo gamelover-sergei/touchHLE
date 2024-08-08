12b8a6a89f148625982070a7c69a3348596dc18b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-08 19:59:27 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -370,6 +370,9 @@ fn abs(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn asctime(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn dladdr(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn gzopen(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
@@ -730,6 +733,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abort(_, _)),
     export_c_func!(abs(_, _)),
     export_c_func!(asctime(_, _)),
+    export_c_func!(dladdr(_, _)),
     export_c_func!(gzopen(_, _)),
     export_c_func!(gzread(_, _)),
     export_c_func!(gzclose(_, _)),
