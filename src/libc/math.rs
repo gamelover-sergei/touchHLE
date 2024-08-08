e60a50367e3d06eedf191689d58ebae2f8c8e060
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-08 16:00:43 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -289,6 +289,9 @@ fn CGLayerGetContext(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn NSFullUserName(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn SCNetworkReachabilitySetCallback(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn UIApplicationDidReceiveMemoryWarningNotification(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -688,6 +691,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGLayerCreateWithContext(_, _)),
     export_c_func!(CGLayerGetContext(_, _)),
     export_c_func!(NSFullUserName(_, _)),
+    export_c_func!(SCNetworkReachabilitySetCallback(_, _)),
     export_c_func!(UIApplicationDidReceiveMemoryWarningNotification(_, _)),
     export_c_func!(glDrawTexiOES(_, _)),
     export_c_func!(glBindFramebuffer(_, _)),
