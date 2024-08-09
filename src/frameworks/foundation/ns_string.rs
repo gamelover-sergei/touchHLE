c06e20801d749405376517d37c954b2968c79917
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-09 16:48:11 +0200

    Update ns_string.rs

--- src/frameworks/foundation/ns_string.rs
+++ src/frameworks/foundation/ns_string.rs
@@ -1171,10 +1171,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     this
 }
 
-- (id)initWithCharacters:(id)characters:(id)length {
-    nil
-}
-
 - (id)initWithString:(id)string { // NSString *
     // TODO: optimize for more common cases (or maybe just call copy?)
     let mut code_units = Vec::new();
