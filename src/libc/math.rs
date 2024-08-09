04f585c40f97fdc11b7a7fdded14a203eaa0ebed
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-09 12:38:03 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -295,6 +295,9 @@ fn CGLayerCreateWithContext(_env: &mut Environment, arg1: f32, arg2: f32) -> f32
 fn CGLayerGetContext(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn MFMailComposeErrorDomain(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn NSFullUserName(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -723,6 +726,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGImageCreateWithImageInRect(_, _)),
     export_c_func!(CGLayerCreateWithContext(_, _)),
     export_c_func!(CGLayerGetContext(_, _)),
+    export_c_func!(MFMailComposeErrorDomain(_, _)),
     export_c_func!(NSFullUserName(_, _)),
     export_c_func!(SCNetworkReachabilitySetCallback(_, _)),
     export_c_func!(SCNetworkReachabilityScheduleWithRunLoop(_, _)),
