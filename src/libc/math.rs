2c48ec796998caace175b4c6f27a1f3c4a4275a2
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-12 11:19:41 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -433,6 +433,9 @@ fn glReadPixels(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn glPointSizePointerOES(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn glIsBuffer(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn abort(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -850,6 +853,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glGetTexParameteriv(_, _)),
     export_c_func!(glReadPixels(_, _)),
     export_c_func!(glPointSizePointerOES(_, _)),
+    export_c_func!(glIsBuffer(_, _)),
     export_c_func!(abort(_, _)),
     export_c_func!(abs(_, _)),
     export_c_func!(asctime(_, _)),
