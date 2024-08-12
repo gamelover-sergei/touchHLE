52a6be72e29609bb088c2cdac2a8a3183b0d3828
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-12 14:56:18 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -271,6 +271,9 @@ fn CCCrypt(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn CC_MD5_Init(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CC_MD5_Update(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CFAbsoluteTimeGetDifferenceAsGregorianUnits(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -814,6 +817,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CACurrentMediaTime(_, _)),
     export_c_func!(CCCrypt(_, _)),
     export_c_func!(CC_MD5_Init(_, _)),
+    export_c_func!(CC_MD5_Update(_, _)),
     export_c_func!(CFAbsoluteTimeGetDifferenceAsGregorianUnits(_, _)),
     export_c_func!(CFAllocatorGetDefault(_, _)),
     export_c_func!(CFBundleCopyResourceURLForLocalization(_, _)),
