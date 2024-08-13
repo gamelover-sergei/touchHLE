0e16880eb814e2832f8a474333fc3c411f21ed1a
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-13 20:01:16 +0200

    Update ns_array.rs

--- src/frameworks/foundation/ns_array.rs
+++ src/frameworks/foundation/ns_array.rs
@@ -305,11 +305,6 @@ pub const CLASSES: ClassExports = objc_classes! {
     env.objc.dealloc_object(this, &mut env.mem)
 }
 
-- (id)writeToFile:(id)file 
-       atomically:(bool)_atomically {
-    // TODO
-}
-
 - (id)arrayByAddingObject {
     nil
 }
