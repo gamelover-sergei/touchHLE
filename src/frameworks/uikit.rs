ff4d0bb4514eb30d903d2eed40134d7339cbc71c
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-14 09:43:06 +0200

    Update uikit.rs

--- src/frameworks/uikit.rs
+++ src/frameworks/uikit.rs
@@ -29,6 +29,7 @@ pub mod ui_image;
 pub mod ui_image_picker_controller;
 pub mod ui_navigation_controller;
 pub mod ui_nib;
+pub mod ui_page_control;
 pub mod ui_paste_board;
 pub mod ui_progress_view;
 pub mod ui_responder;
