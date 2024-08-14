1d83306a0c958c85514393415781dfc1c50e93a0
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-14 10:36:44 +0200

    Update ui_page_control.rs

--- src/frameworks/uikit/ui_page_control.rs
+++ src/frameworks/uikit/ui_page_control.rs
@@ -5,6 +5,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 (env, this, _cmd);
 
 @implementation UIPageControl: UIControl
+- (())setNumberOfPages:(bool)pages {
+    log!("TODO: setNumberOfPages:{}", paged);
+}
 
 @end
 
