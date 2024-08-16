b598f3ad87339393a251338cee828a7e75b6ca59
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-16 22:46:42 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -484,6 +484,9 @@ fn asctime(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn class_getInstanceSize(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn ctime(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn dladdr(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -939,6 +942,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(abs(_, _)),
     export_c_func!(asctime(_, _)),
     export_c_func!(class_getInstanceSize(_, _)),
+    export_c_func!(ctime(_, _)),
     export_c_func!(dladdr(_, _)),
     export_c_func!(fts_open(_, _)),
     export_c_func!(fts_read(_, _)),
