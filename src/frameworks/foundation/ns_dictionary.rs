50028b7292d98ac6b3d466f9b3738aaf89412904
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-17 10:09:25 +0200

    Update ns_dictionary.rs

--- src/frameworks/foundation/ns_dictionary.rs
+++ src/frameworks/foundation/ns_dictionary.rs
@@ -456,6 +456,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 @implementation CLLocationManager: NSMutableDictionary
 @end
 
+@implementation CMMotionManager: NSMutableDictionary
+@end
+
 @implementation NSCondition: NSMutableDictionary
 @end
 
