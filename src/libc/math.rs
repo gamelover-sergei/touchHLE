91af97af5658ffb8da2bc3e55f21b2f4a36b884d
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-20 08:15:16 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -322,6 +322,9 @@ fn CFURLCreateStringByReplacingPercentEscapesUsingEncoding(_env: &mut Environmen
 fn CFURLGetString(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CFURLCreateWithString(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CGColorEqualToColor(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -909,6 +912,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFStringGetSystemEncoding(_, _)),
     export_c_func!(CFURLCreateStringByReplacingPercentEscapesUsingEncoding(_, _)),
     export_c_func!(CFURLGetString(_, _)),
+    export_c_func!(CFURLCreateWithString(_, _)),
     export_c_func!(CGColorEqualToColor(_, _)),
     export_c_func!(CGColorGetConstantColor(_, _)),
     export_c_func!(CGContextClipToRect(_, _)),
