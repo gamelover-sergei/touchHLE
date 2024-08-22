753aa060ae412772045c5e304c0e7d99681acbb8
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-22 17:10:18 +0200

    Update ui_slider.rs

--- src/frameworks/uikit/ui_view/ui_control/ui_slider.rs
+++ src/frameworks/uikit/ui_view/ui_control/ui_slider.rs
@@ -30,6 +30,10 @@ pub const CLASSES: ClassExports = objc_classes! {
     log!("TODO: setValue:{}", value);
 }
 
+- (())setMinimumValueImage:(bool)image {
+    log!("TODO: setMinimumValueImage:{}", image);
+}
+
 - (())setThumbImage:(NSInteger)image forState:(bool)_state {
     // TODO
 }
