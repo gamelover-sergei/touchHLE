78efaab1b539333c72f5bb7fd91632b875118679
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-22 15:20:47 +0200

    Update ui_switch.rs

--- src/frameworks/uikit/ui_switch.rs
+++ src/frameworks/uikit/ui_switch.rs
@@ -1,10 +1,14 @@
-use crate::objc::{objc_classes, ClassExports};
+use crate::objc::{id, nil, objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
 
 (env, this, _cmd);
 
 @implementation UISwitch: UIControl
+
+- (id)isOn {
+    nil
+}
 
 - (())setOn:(bool)on {
     log!("TODO: setOn:{}", on);
