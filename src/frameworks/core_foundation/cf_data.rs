5164d6e6cd8b91d18d1558652f692aa24d870ff0
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-22 13:31:06 +0200

    Update cf_data.rs

--- src/frameworks/core_foundation/cf_data.rs
+++ src/frameworks/core_foundation/cf_data.rs
@@ -34,7 +34,7 @@ pub fn CFDataCreate(
 
 fn CFDataGetLength(env: &mut Environment, data: CFDataRef) -> CFIndex {
     let len: NSUInteger = msg![env; data length];
-    len.try_into()
+    len.try_into().unwrap()
 }
 
 fn CFDataGetBytePtr(env: &mut Environment, data: CFDataRef) -> ConstPtr<u8> {
