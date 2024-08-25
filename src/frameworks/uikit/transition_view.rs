481609596237b82748923449f471c16b2569691f
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-25 10:58:08 +0200

    Create transition_view.rs

---
+++ src/frameworks/uikit/transition_view.rs
@@ -0,0 +1,15 @@
+use crate::objc::{objc_classes, ClassExports};
+
+pub const CLASSES: ClassExports = objc_classes! {
+
+(env, this, _cmd);
+
+@implementation TransitionView: UIView
+
+- (())insertSubview:(NSInteger)subview atIndex:(bool)_index {
+    // TODO
+}
+
+@end
+
+};
