d88df8fa43c3d770015b3ba2afa92abce42f22c7
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-26 14:32:41 +0200

    Update environment.rs

--- src/environment.rs
+++ src/environment.rs
@@ -819,7 +819,6 @@ impl Environment {
         self.stack_trace();
         self.gdb_server
             .as_mut()
-            .unwrap()
             .wait_for_debugger(reason, &mut self.cpu, &mut self.mem)
     }
 
