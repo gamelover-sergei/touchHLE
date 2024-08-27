85feec7a229a000b74d416cb38d4fff6065e9336
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-27 14:05:11 +0200

    Update cf_bundle.rs

--- src/frameworks/core_foundation/cf_bundle.rs
+++ src/frameworks/core_foundation/cf_bundle.rs
@@ -67,8 +67,8 @@ fn CFBundleGetVersionNumber(env: &mut Environment, bundle: CFBundleRef) -> u32 {
     result
 }
 
-fn CFBundleCopyExecutableURL(env: &mut Environment, bundle: CFBundleRef) -> CFURLRef {
-    let url: id = msg![env; bundle executableURL];
+fn CFBundleCopyBundleURL(env: &mut Environment, bundle: CFBundleRef) -> CFURLRef {
+    let url: CFURLRef = msg![env; bundle bundleURL];
     msg![env; url copy]
 }
 
@@ -158,7 +158,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFBundleGetMainBundle()),
     export_c_func!(CFBundleGetValueForInfoDictionaryKey(_, _)),
     export_c_func!(CFBundleGetVersionNumber(_)),
-    export_c_func!(CFBundleCopyExecutableURL(_)),
+    export_c_func!(CFBundleCopyBundleURL(_)),
     export_c_func!(CFBundleCopyResourcesDirectoryURL(_)),
     export_c_func!(CFBundleCopyResourceURL(_, _, _, _)),
     export_c_func!(CFBundleCopyBundleLocalizations(_)),
