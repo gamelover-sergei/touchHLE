3f6af839c7b6b07303c6f850ab17fea358dd23c6
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-28 15:48:34 +0200

    Update ns_value.rs

--- src/frameworks/foundation/ns_value.rs
+++ src/frameworks/foundation/ns_value.rs
@@ -40,6 +40,14 @@ pub const CLASSES: ClassExports = objc_classes! {
     autorelease(env, new)
 }
 
++ (id)valueWithCGPoint:(u64)value {
+    // TODO: for greater efficiency we could return a static-lifetime value
+
+    let new: id = msg![env; this alloc];
+    let new: id = msg![env; new initWithUnsignedLongLong:value];
+    autorelease(env, new)
+}
+
 // NSCopying implementation
 - (id)copyWithZone:(NSZonePtr)_zone {
     retain(env, this)
