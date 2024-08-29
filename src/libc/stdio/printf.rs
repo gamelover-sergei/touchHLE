2eb2a433799ee5e5e6ba9adbe24264ba20aecf90
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-29 08:44:43 +0200

    Update printf.rs

--- src/libc/stdio/printf.rs
+++ src/libc/stdio/printf.rs
@@ -239,6 +239,11 @@ pub fn printf_inner<const NS_LOG: bool, F: Fn(&Mem, GuestUSize) -> u8>(
                 res.extend_from_slice(format!("{:?}", ptr).as_bytes());
             }
             // Float specifiers
+            b'#' => {
+                // assert!(length_modifier.is_none());
+                let ptr: MutVoidPtr = args.next(env);
+                res.extend_from_slice(format!("{:?}", ptr).as_bytes());
+            }
             b'f' => {
                 let float: f64 = args.next(env);
                 let pad_width = pad_width as usize;
