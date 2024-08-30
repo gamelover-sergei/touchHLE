bf3d97892b4f2508e88714c9eb72fe60c8b0b61e
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-30 08:49:04 +0200

    Update gk_session.rs

--- src/frameworks/uikit/gk_session.rs
+++ src/frameworks/uikit/gk_session.rs
@@ -1,3 +1,4 @@
+use crate::frameworks::foundation::NSUInteger;
 use crate::objc::{objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
@@ -5,6 +6,9 @@ pub const CLASSES: ClassExports = objc_classes! {
 (env, this, _cmd);
 
 @implementation GKSession: NSObject
+- (())initWithSessionID:(NSInteger)session displayName:(bool)_name sessionMode:(bool)mode {
+    // TODO
+}
 
 @end
 
