8d07e221c932ffea4a896e2131d26aa8eb8b7c63
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-30 08:34:57 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -460,6 +460,9 @@ fn _NSSetLogCStringFunction(_env: &mut Environment, arg1: f32, arg2: f32) -> f32
 fn _Unwind_SjLj_Register(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn _Unwind_SjLj_Unregister(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn glDrawTexiOES(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1018,6 +1021,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(UIGraphicsGetImageFromCurrentImageContext(_, _)),
     export_c_func!(_NSSetLogCStringFunction(_, _)),
     export_c_func!(_Unwind_SjLj_Register(_, _)),
+    export_c_func!(_Unwind_SjLj_Unregister(_, _)),
     export_c_func!(glDrawTexiOES(_, _)),
     export_c_func!(glBindFramebuffer(_, _)),
     export_c_func!(glBlendEquationOES(_, _)),
