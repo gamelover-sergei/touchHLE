b4c1b018bf849fef7b4a4804cdbbde69bef527aa
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-08-31 09:39:21 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -367,6 +367,9 @@ fn CGContextAddRect(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn CGContextClipToRect(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CGContextClosePath(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CGContextDrawPath(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1011,6 +1014,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGContextAddLineToPoint(_, _)),
     export_c_func!(CGContextAddRect(_, _)),
     export_c_func!(CGContextClipToRect(_, _)),
+    export_c_func!(CGContextClosePath(_, _)),
     export_c_func!(CGContextDrawPath(_, _)),
     export_c_func!(CGContextFillEllipseInRect(_, _)),
     export_c_func!(CGContextMoveToPoint(_, _)),
