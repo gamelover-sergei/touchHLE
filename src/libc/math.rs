385cbf104cae866c4a08b393f03aca92c1b767a0
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-03 18:35:18 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -340,6 +340,9 @@ fn CFSetCreateMutable(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn CFStringCreateWithFileSystemRepresentation(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CFStringGetBytes(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CFStringGetCharacterAtIndex(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1023,6 +1026,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFRunLoopRun(_, _)),
     export_c_func!(CFSetCreateMutable(_, _)),
     export_c_func!(CFStringCreateWithFileSystemRepresentation(_, _)),
+    export_c_func!(CFStringGetBytes(_, _)),
     export_c_func!(CFStringGetCharacterAtIndex(_, _)),
     export_c_func!(CFStringGetSystemEncoding(_, _)),
     export_c_func!(CFStringHasSuffix(_, _)),
