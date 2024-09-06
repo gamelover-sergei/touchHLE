72e62f9c6603d6afbc11ef4d068fae6f8d4d1617
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-06 19:56:23 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -334,6 +334,9 @@ fn CFBundleCopyExecutableURL(_env: &mut Environment, arg1: f32, arg2: f32) -> f3
 fn CFLocaleCopyCurrent(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CFRunLoopAddSource(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CFRunLoopSourceCreate(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1036,6 +1039,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CFBundleGetInfoDictionary(_, _)),
     export_c_func!(CFBundleCopyExecutableURL(_, _)),
     export_c_func!(CFLocaleCopyCurrent(_, _)),
+    export_c_func!(CFRunLoopAddSource(_, _)),
     export_c_func!(CFRunLoopRun(_, _)),
     export_c_func!(CFRunLoopSourceCreate(_, _)),
     export_c_func!(CFSetCreateMutable(_, _)),
