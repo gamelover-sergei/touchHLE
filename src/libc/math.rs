b8ec35c44491b4503f398acb99082ea782bc72ef
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-07 08:02:36 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -634,6 +634,9 @@ fn gzclose(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn gzeof(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn herror(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 fn inflate(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
@@ -1139,6 +1142,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(gzread(_, _)),
     export_c_func!(gzclose(_, _)),
     export_c_func!(gzeof(_, _)),
+    export_c_func!(herror(_, _)),
     export_c_func!(inflate(_, _)),
     export_c_func!(inflateInit_(_, _)),
     export_c_func!(inflateInit2_(_, _)),
