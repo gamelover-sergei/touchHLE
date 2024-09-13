c30f526ba16d95670977b7dcc8461f1ee82cd8d2
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-13 10:48:53 +0200

    Update messages.rs

--- src/objc/messages.rs
+++ src/objc/messages.rs
@@ -31,7 +31,6 @@ use std::any::TypeId;
 /// overwriting it.
 #[allow(non_snake_case)]
 fn objc_msgSend_inner(env: &mut Environment, receiver: id, selector: SEL, super2: Option<Class>) {
-    log_dbg!("Dispatching {} for {:?}", selector.as_str(&env.mem), receiver);
     let message_type_info = env.objc.message_type_info.take();
 
     if receiver == nil {
@@ -90,7 +89,6 @@ fn objc_msgSend_inner(env: &mut Environment, receiver: id, selector: SEL, super2
         if let Some(&super::ClassHostObject {
             superclass,
             ref methods,
-            ref ivars,
             ref name,
             ..
         }) = host_object.as_any().downcast_ref()
@@ -103,7 +101,7 @@ fn objc_msgSend_inner(env: &mut Environment, receiver: id, selector: SEL, super2
             }
 
             if let Some(imp) = methods.get(&selector) {
-                log_dbg!("Found method on: {}", name);
+                // log!("Found method on: {}", name);
                 match imp {
                     IMP::Host(host_imp) => {
                         // TODO: do type checks when calling GuestIMPs too.
@@ -133,14 +131,6 @@ Type mismatch when sending message {} to {:?}!
                     // interfere with pass-through of stack arguments.
                     IMP::Guest(guest_imp) => guest_imp.call_without_pushing_stack_frame(env),
                 }
-                return;
-            } else if let Some(ivar_offset_ptr) = ivars.get(&selector) {
-                let ivar_offset = env.mem.read(*ivar_offset_ptr);
-                // TODO: Use host_object's _instance_start property?
-                let ivar_ptr = MutVoidPtr::from_bits(receiver.to_bits() + ivar_offset);
-                //let value = env.cpu.regs()[0];
-                //env.mem.write(ivar_ptr.cast(), ivar_ptr.to_bits());
-                objc_msgSend_inner(env, ivar_ptr.cast(), selector, None);
                 return;
             } else {
                 class = superclass;
