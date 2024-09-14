e1a3716ae6e2a8635bd0c9a8d7d06f6e9eeeee85
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-15 00:00:30 +0200

    Update objects.rs

--- src/objc/objects.rs
+++ src/objc/objects.rs
@@ -23,6 +23,7 @@
 use super::{Class, ClassHostObject};
 use crate::mem::{guest_size_of, GuestUSize, Mem, MutPtr, Ptr, SafeRead};
 use std::any::Any;
+use std::borrow::BorrowMut;
 
 /// Memory layout of a minimal Objective-C object. See [id].
 ///
@@ -256,7 +257,7 @@ impl super::ObjC {
         // through a data structure with a mutable borrow. The unsafe code is
         // used to bypass the borrow checker.
         type Aho = dyn AnyHostObject + 'static;
-        let mut host_object: &mut Aho = &mut *self.objects.get_mut(&object).unwrap().host_object;
+        let mut host_object: &mut Aho = &mut *self.objects.get_mut(&object).borrow_mut().host_object;
         loop {
             if let Some(res) = unsafe { &mut *(host_object as *mut Aho) }
                 .as_any_mut()
