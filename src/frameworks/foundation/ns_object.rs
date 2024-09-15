ba01649c4084ef598bc42d9ef7d17854e96c7d40
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-15 11:35:06 +0200

    Update ns_object.rs

--- src/frameworks/foundation/ns_object.rs
+++ src/frameworks/foundation/ns_object.rs
@@ -283,7 +283,7 @@ forUndefinedKey:(id)key { // NSString*
     let isa = ObjC::read_isa(this, &env.mem);
     match ObjC::lookup_method_imp(env, isa, selector) {
         IMP::Guest(g) => ConstVoidPtr::from_bits(g.addr_with_thumb_bit()),
-        IMP::Host(_) => false!
+        IMP::Host(_) => false,
     }
 }
     
