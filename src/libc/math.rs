27a75d14747f075d33884312f3ace4519b2c47d5
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-15 16:49:55 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -472,6 +472,9 @@ fn CGContextSetShadow(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
 fn CGContextSetShouldAntialias(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn CGContextSetShouldSmoothFonts(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn CGContextSetTextPosition(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1145,6 +1148,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(CGContextSetRGBStrokeColor(_, _)),
     export_c_func!(CGContextSetShadow(_, _)),
     export_c_func!(CGContextSetShouldAntialias(_, _)),
+    export_c_func!(CGContextSetShouldSmoothFonts(_, _)),
     export_c_func!(CGContextSetTextPosition(_, _)),
     export_c_func!(CGContextStrokeEllipseInRect(_, _)),
     export_c_func!(CGDataProviderCreateDirect(_, _)),
