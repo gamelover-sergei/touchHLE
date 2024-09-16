9e1fa7552a84abeed8bd7676c9e3ca3e3dbe2f6a
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-16 21:52:36 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -646,6 +646,9 @@ fn glRenderbufferStorageMultisampleAPPLE(_env: &mut Environment, arg1: f32, arg2
 fn glDrawTexfOES(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
+fn glDeleteShader(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
+    arg1.min(arg2)
+}
 fn abort(_env: &mut Environment, arg1: f32, arg2: f32) -> f32 {
     arg1.min(arg2)
 }
@@ -1218,6 +1221,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(glGetRenderbufferParameteriv(_, _)),
     export_c_func!(glRenderbufferStorageMultisampleAPPLE(_, _)),
     export_c_func!(glDrawTexfOES(_, _)),
+    export_c_func!(glDeleteShader(_, _)),
     export_c_func!(abort(_, _)),
     export_c_func!(abs(_, _)),
     export_c_func!(asctime(_, _)),
