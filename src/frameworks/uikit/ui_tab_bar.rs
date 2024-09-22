ad10df54b8e5de4f0b30e32869047d4ade82725b
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-22 16:05:24 +0200

    Update ui_tab_bar.rs

--- src/frameworks/uikit/ui_tab_bar.rs
+++ src/frameworks/uikit/ui_tab_bar.rs
@@ -1,3 +1,4 @@
+use crate::frameworks::foundation::NSInteger;
 use crate::objc::{objc_classes, ClassExports};
 
 pub const CLASSES: ClassExports = objc_classes! {
@@ -5,6 +6,11 @@ pub const CLASSES: ClassExports = objc_classes! {
 (env, this, _cmd);
 
 @implementation UITabBar: UIControl
+
+- (())setItems:(NSInteger)items animated:(bool)_animated {
+  // TODO
+}
+
 
 @end
 
