adcb141e03608a9e4569aed41e326c43dceb1c3f
SonicVrey <158614150+SonicVrey@users.noreply.github.com>
2024-09-29 22:12:31 +0200

    Update math.rs

--- src/libc/math.rs
+++ src/libc/math.rs
@@ -913,6 +913,9 @@ fn sqlite3_bind_text(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
 fn sqlite3_column_text(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
+fn sqlite3_last_insert_rowid(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
+    arg1.min(arg2)
+}
 fn sranddev(_env: &mut Environment, arg1: f64, arg2: f64) -> f64 {
     arg1.min(arg2)
 }
@@ -1367,6 +1370,7 @@ pub const FUNCTIONS: FunctionExports = &[
     export_c_func!(sqlite3_column_int(_, _)),
     export_c_func!(sqlite3_bind_text(_, _)),
     export_c_func!(sqlite3_column_text(_, _)),
+    export_c_func!(sqlite3_last_insert_rowid(_, _)),
     export_c_func!(sranddev(_, _)),
     export_c_func!(srandomdev(_, _)),
     export_c_func!(strpbrk(_, _)),
